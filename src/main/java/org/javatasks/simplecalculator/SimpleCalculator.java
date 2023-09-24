10c1c95328b45c25181eae15a22590490b62e020
Emmanuel Xs <emmanuelnwaohiri1@gmail.com>
2023-09-24 19:39:47 +0100

    Code review

--- src/main/java/org/javatasks/simplecalculator/SimpleCalculator.java
+++ src/main/java/org/javatasks/simplecalculator/SimpleCalculator.java
@@ -10,65 +10,72 @@ public class SimpleCalculator {
         int attempts = 5;
 
         System.out.println("Simple Calculator");
+        char recur;
 
-        double firstNumber = getUserInput("Enter the first number: ");
-        double secondNumber = getUserInput("Enter the second number: ");
-        int operationChoice;
+        do {
+            double firstNumber = getUserInput("Enter the first number: ");
+            double secondNumber = getUserInput("Enter the second number: ");
+            int operationChoice;
 
-        while (true) {
-            System.out.println("Choose an operation:");
-            System.out.println("1. Addition");
-            System.out.println("2. Subtraction");
-            System.out.println("3. Multiplication");
-            System.out.println("4. Division");
-            System.out.print("Enter the number corresponding to the operation: ");
+            while (true) {
+                System.out.println("Choose an operation:");
+                System.out.println("1. Addition");
+                System.out.println("2. Subtraction");
+                System.out.println("3. Multiplication");
+                System.out.println("4. Division");
+                System.out.print("Enter the number corresponding to the operation: ");
 
-            if (scanner.hasNextInt()) {
-                operationChoice = scanner.nextInt();
-                if (operationChoice >= 1 && operationChoice <= 4) {
-                    break;
+                if (scanner.hasNextInt()) {
+                    operationChoice = scanner.nextInt();
+                    if (operationChoice >= 1 && operationChoice <= 4) {
+                        break;
+                    } else {
+                        System.out.println("Invalid operation choice. Please enter a number between 1 and 4.");
+                        scanner.nextLine(); // Clear the input buffer
+                    }
                 } else {
-                    System.out.println("Invalid operation choice. Please enter a number between 1 and 4.");
+                    System.out.println("Invalid input. Please enter a valid number.");
                     scanner.nextLine(); // Clear the input buffer
                 }
-            } else {
-                System.out.println("Invalid input. Please enter a valid number.");
-                scanner.nextLine(); // Clear the input buffer
-            }
 
-            attempts--;
+                attempts--;
 
-            if (attempts == 0) {
-                System.out.println("You've reached the maximum number of attempts. Exiting the program.");
-                System.exit(1);
-            }
-        }
-
-        double result = 0.0;
-
-        switch (operationChoice) {
-            case 1:
-                result = firstNumber + secondNumber;
-                break;
-            case 2:
-                result = firstNumber - secondNumber;
-                break;
-            case 3:
-                result = firstNumber * secondNumber;
-                break;
-            case 4:
-                if (secondNumber != 0) {
-                    result = firstNumber / secondNumber;
-                } else {
-                    System.out.println("Error: Division by zero is not allowed.");
+                if (attempts == 0) {
+                    System.out.println("You've reached the maximum number of attempts. Exiting the program.");
                     System.exit(1);
                 }
-                break;
-            default:
-                break;
-        }
+            }
 
-        System.out.println("Result: " + result);
+            double result = 0.0;
+
+            switch (operationChoice) {
+                case 1:
+                    result = firstNumber + secondNumber;
+                    break;
+                case 2:
+                    result = firstNumber - secondNumber;
+                    break;
+                case 3:
+                    result = firstNumber * secondNumber;
+                    break;
+                case 4:
+                    if (secondNumber != 0) {
+                        result = firstNumber / secondNumber;
+                    } else {
+                        System.out.println("Error: Division by zero is not allowed.");
+                        System.exit(1);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            System.out.println("Result: " + result);
+
+            System.out.println("Do you wish to perform another operation? Enter y for YES; any other key means NO\n");
+            recur = scanner.next().charAt(0);
+
+        } while (recur == 'y');
     }
 
     private static double getUserInput(String prompt) {
